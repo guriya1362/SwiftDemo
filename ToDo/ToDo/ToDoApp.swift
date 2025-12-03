cfb06182e81f6fe225b5f2e4ee3ddeae7d1216dc
Guriya Kumari <gkumari@phunware.com>
2025-12-03 10:43:23 +0530

    create Project with initial setup

---
+++ ToDo/ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Guriya Kumari on 03/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
